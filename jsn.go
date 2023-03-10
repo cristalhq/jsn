28ea1a8713c74bc8203fcc4b26f258baca590332
Oleg Kovalov <oleg@hey.com>
2023-03-10 17:06:27 +0100

    Add JSON number

--- jsn.go
+++ jsn.go
@@ -5,3 +5,10 @@ type A = []any
 
 // O represents JSON object.
 type O = map[string]any
+
+// N represents JSON number.
+type N string
+
+func (n N) MarshalJSON() ([]byte, error) {
+	return []byte(n), nil
+}
